a6ef4a2cf1768eed23e108b4f3eae0990dd118ae
Stephanie <min_T_steef@hotmail.com>
2025-08-14 13:32:26 +0200

    fix: input error

--- src/app/shared/input/input.component.ts
+++ src/app/shared/input/input.component.ts
@@ -7,7 +7,13 @@ import {
   Output,
   SkipSelf,
 } from "@angular/core";
-import { ControlContainer, Form, FormControl, FormGroup } from "@angular/forms";
+import {
+  AbstractControl,
+  ControlContainer,
+  Form,
+  FormControl,
+  FormGroup,
+} from "@angular/forms";
 import { DropdownInterface } from "../../types/dropdown.interface";
 
 @Component({
@@ -19,11 +25,23 @@ export class InputComponent {
   @Input() label?: string;
   @Input() control!: string;
   @Input() type: string = "text";
+  error: string = "Uw kat moet zwaarder zijn dan 0.1kg";
 
   constructor(
     @Optional() @SkipSelf() private controlContainer: ControlContainer
   ) {}
   get formGroup(): FormGroup {
     return this.controlContainer.control as FormGroup;
+  }
+  get formControl(): AbstractControl | null {
+    return this.formGroup.get(this.control);
+  }
+
+  controlHasError(): boolean {
+    return !!(
+      this.formControl &&
+      this.formControl.invalid &&
+      (this.formControl.dirty || this.formControl.touched)
+    );
   }
 }
